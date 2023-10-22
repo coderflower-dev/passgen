3967c9a373cec7775d3dd7d5c7675c55063b91eb
DrFlower <90166542+therealdrflower@users.noreply.github.com>
2023-10-22 18:58:27 -0500

    Update passgen.cpp

--- passgen.cpp
+++ passgen.cpp
@@ -10,28 +10,36 @@ void generate()
 {
   std::string pass;
   int length = 10;
+  //starts up random machine
   srand(time(NULL));
+  //main loop
   for(int i = 0; i<length; i++)
   {
   uint8_t choice = rand() % 10;
+    //this generates a random letter
   if(choice <= 5)
   {
      //Random letter
 
      char letter;
      letter = 'a' + rand() % 26;
+    //checks if it is a letter
      if(isalpha(letter))
      {
+       //adds letter to password
        pass += letter;
      }
   }
+    //generates random number
   if(choice >= 6)
   {
     //Random number
     int number;
     number = rand() % 100;
+    //checks if number
     if(isdigit(char(number)))
      {
+       //adds number to password
        pass += number;
      }
   }
@@ -43,6 +51,7 @@ int main()
 {
   std::cout << "Created by Timur G." << std::endl;
   std::cout << "============" << std::endl;
+  //generates the password
   generate();
   std::cout << "============" << std::endl;
   return 0;
